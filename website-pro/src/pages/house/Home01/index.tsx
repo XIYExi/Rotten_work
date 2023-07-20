2d066f86f473aa74960d1cb5ad444d2a4e936813
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-20 19:48:12 +0800

    update：添加搜索框

--- website-pro/src/pages/house/Home01/index.tsx
+++ website-pro/src/pages/house/Home01/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePageIndex from "@/pages/house/Home01/HomePage";
+import SearchBarIndex from "@/pages/house/Home01/SearchBar";
 
 export default function Home01() {
 
@@ -7,6 +8,8 @@ export default function Home01() {
     <>
       {/*首页*/}
       <HomePageIndex />
+      {/*搜索条*/}
+      <SearchBarIndex />
 
     </>
   )
