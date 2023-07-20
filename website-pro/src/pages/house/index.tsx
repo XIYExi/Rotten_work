65e76605e923e6fbf87326220efbdc326e45275a
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-20 16:45:22 +0800

    delete：删除测试代码

--- website-pro/src/pages/house/index.tsx
+++ website-pro/src/pages/house/index.tsx
@@ -8,7 +8,6 @@ export default function IndexPage({children}: IRouteProps) {
     <div className='overflow-x-hidden'>
       <HeaderComponent />
       {children}
-      <div style={{width:'100%',height:'100dvh'}}/>
       <FooterComponent />
     </div>
   );
