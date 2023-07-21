45d0a6a4bd877aabbef0b0ac5ab263d89f4f7875
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-21 12:06:57 +0800

    update：部分页面修改名称并添加注释

---
+++ website-pro/src/pages/house/Home01/BestChoice.tsx
@@ -0,0 +1,61 @@
+/**
+ * @Description: 最佳推荐
+ * @Author: xiye
+ * @date: 2023/7/21
+ */
+import React, {Fragment} from "react";
+
+export default function BestChoicePage() {
+
+  return (
+    <section>
+      <div>Best Choice</div>
+      <div className='flex flex-wrap'>
+        <div >Popular Properties</div>
+        <div>Explore all</div>
+      </div>
+
+      <div>
+
+      </div>
+
+    </section>
+  )
+}
+
+interface CardProps {
+  src:string;
+  title:string;
+  address: string;
+  date: string;
+  price: number;
+  meta:string;
+}
+
+const Card = (props: CardProps) => {
+
+  const {
+    src,
+    title,
+    address,
+    date,
+    price,
+    meta
+  } = props;
+
+  return(
+    <Fragment>
+      <div>
+        <div className='text-center'>
+          <img src={src} />
+          <div>{title}</div>
+          <div >{address}</div>
+          <div>Added: {date}</div>
+          <div>Price:${price}</div>
+          <div>{meta}</div>
+
+        </div>
+      </div>
+    </Fragment>
+  )
+}
