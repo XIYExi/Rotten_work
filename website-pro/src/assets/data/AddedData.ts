353f050966d2ac460fb3f9efd6c04fffdfa76dbf
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-22 11:07:35 +0800

    add：添加新添加房屋数据

---
+++ website-pro/src/assets/data/AddedData.ts
@@ -0,0 +1,262 @@
+const Properties = [
+  {
+    src: '1',
+    title: 'Orchid Casel de Paradise.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '2',
+    title: 'Ruposibangla de parlosia.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '3',
+    title: 'Sinomen Studio Palace.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '4',
+    title: 'Orchid Casel de Paradise.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '5',
+    title: 'Ruposibangla de parlosia.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '6',
+    title: 'Sinomen Studio Palace.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+]
+
+const Buy = [
+  {
+    src: '3',
+    title: 'Orchid Casel de Paradise.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '5',
+    title: 'Ruposibangla de parlosia.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '8',
+    title: 'Sinomen Studio Palace.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '4',
+    title: 'Orchid Casel de Paradise.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '1',
+    title: 'Ruposibangla de parlosia.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '6',
+    title: 'Sinomen Studio Palace.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+]
+
+const Sale = [
+  {
+    src: '8',
+    title: 'Orchid Casel de Paradise.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '3',
+    title: 'Ruposibangla de parlosia.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '4',
+    title: 'Sinomen Studio Palace.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '7',
+    title: 'Orchid Casel de Paradise.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '1',
+    title: 'Ruposibangla de parlosia.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '2',
+    title: 'Sinomen Studio Palace.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+]
+
+const Rent = [
+  {
+    src: '1',
+    title: 'Orchid Casel de Paradise.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '3',
+    title: 'Ruposibangla de parlosia.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '2',
+    title: 'Sinomen Studio Palace.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '4',
+    title: 'Orchid Casel de Paradise.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '8',
+    title: 'Ruposibangla de parlosia.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '7',
+    title: 'Sinomen Studio Palace.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+]
+
+const Co = [
+  {
+    src: '3',
+    title: 'Orchid Casel de Paradise.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '2',
+    title: 'Ruposibangla de parlosia.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '1',
+    title: 'Sinomen Studio Palace.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '6',
+    title: 'Orchid Casel de Paradise.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '5',
+    title: 'Ruposibangla de parlosia.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+  {
+    src: '4',
+    title: 'Sinomen Studio Palace.',
+    address: '18B Central Street,San Francisco',
+    date: '25 November,2021',
+    price: 255300,
+    meta: '1230 Sq.fit - 4 Bed - 3 Bath - 2 Garage'
+  },
+]
+
+export default {
+  Properties: Properties,
+  Buy: Buy,
+  Sale: Sale,
+  Rent: Rent,
+  Co: Co
+}
