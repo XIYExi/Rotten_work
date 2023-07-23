40f4836a9e97652ad655671787ba6455733e132e
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-23 18:03:49 +0800

    add-commit：添加数据

---
+++ website-pro/src/assets/data/ExploreCitiesData.ts
@@ -0,0 +1,36 @@
+const Cities = [
+  {
+    title:'New York',
+    src: '1',
+    href: '#',
+    desc: '36 Properties',
+  },
+  {
+    title:'Sun Francisco',
+    src: '2',
+    href: '#',
+    desc: '18 Properties',
+  },
+  {
+    title:'Washington D.C.',
+    src: '3',
+    href: '#',
+    desc: '27 Properties',
+  },
+  {
+    title:'New York',
+    src: '4',
+    href: '#',
+    desc: '56 Properties',
+  },
+  {
+    title: 'Francisco',
+    src: '1',
+    href: '#',
+    desc: '18 Properties',
+  }
+]
+
+export default {
+  Cities: Cities,
+}
