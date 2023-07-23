767ae521bd157a51010b762cb14fbb27c7944b89
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-23 18:04:04 +0800

    add：添加testimonial页面

---
+++ website-pro/src/pages/house/Home01/Testimonial.tsx
@@ -0,0 +1,19 @@
+/**
+ * @Description: 动效卡片页面
+ * @Author: xiye
+ * @date: 2023/7/23
+ */
+import React from "react";
+
+export default function TestimonialPage() {
+
+  return (
+    <section className='md:pt-[50px] bg-no-repeat bg-white relative z-10 custom-testimonial-bg'>
+      <div className='my-container mx-auto'>
+
+
+
+      </div>
+    </section>
+  )
+}
