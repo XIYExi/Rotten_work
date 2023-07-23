71fd31d463e719e251083361438c7756e17aa84c
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-23 22:16:36 +0800

    add：添加testimonial页面数据

---
+++ website-pro/src/assets/data/TestimonialData.ts
@@ -0,0 +1,21 @@
+const TestimonialData = [
+  {
+    src: '1',
+    desc: 'Bary do a great job to find the perfect home. It’s very easy for every one to buy,sell or rent property we belive they continure their great service.',
+    title: 'Oliver Stephen'
+  },
+  {
+    src: '2',
+    desc: 'Bary do a great job to find the perfect home. It’s very easy for every one to buy,sell or rent property we belive they continure their great service.',
+    title: 'Sun Francisco'
+  },
+  {
+    src: '1',
+    desc: 'Bary do a great job to find the perfect home. It’s very easy for every one to buy,sell or rent property we belive they continure their great service.',
+    title: 'Oliver Stephen'
+  }
+]
+
+export default {
+  TestimonialData: TestimonialData
+}
