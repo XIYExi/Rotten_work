01553b92f9c92c10736d167c9f73cd0537f40e3e
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-23 16:28:03 +0800

    commit：完成video tour页面

--- website-pro/src/pages/house/Home01/VideoTour.tsx
+++ website-pro/src/pages/house/Home01/VideoTour.tsx
@@ -3,12 +3,14 @@
  * @Author: xiye
  * @date: 2023/7/22
  */
-import React, {useEffect, useRef} from "react";
+import React, {CSSProperties, Fragment, useEffect, useRef} from "react";
 import {ReactComponent as VideoTourSvg} from '@/assets/image/icon/tour.svg';
 import star from '@/assets/image/video/star.png';
 import shape from '@/assets/image/video/shape.png';
 import video from '@/assets/image/video/video.png';
+import {ReactComponent as VideoDisplayIcon} from "@/assets/image/video/display.svg";
 import Parallax from 'parallax-js';
+import classnames from "classnames";
 
 export default function VideoTourPage() {
 
@@ -55,10 +57,46 @@ export default function VideoTourPage() {
                 <img data-depth={'0.1'} src={video} className='rounded-[24px]' loading='lazy' width='507' height='349' alt='video image'/>
               </div>
 
+              {/*播放icon*/}
+              <DisplayIcon href='#'/>
+
             </div>
           </div>
         </div>
       </div>
     </section>
+  )
+}
+
+interface DisplayIconProps {
+  href: string;
+  className?:string;
+  style?:CSSProperties;
+}
+
+const DisplayIcon = (props:DisplayIconProps) => {
+
+  const {
+    href,
+    className,
+    style
+  } = props;
+
+  const displayLinkClassName = classnames(
+    /*自定义样式*/
+    'play-button display-icon',
+    /*tailwind*/
+    'bg-white text-white hover:text-primary absolute left-0 right-0 mx-auto top-1/2 -translate-y-1/2 hover:scale-105 hover:bg-primary w-[55px] h-[55px]',
+    'flex flex-wrap z-10 items-center justify-center opacity-80 drop-shadow-modify transition-all rounded-full group',
+    'before:block before:absolute before:bg-white before:opacity-80 before:drop-shadow-modify hover:before:bg-primary hover:before:opacity-80 before:w-[70px] before:h-[70px] before:rounded-full',
+    className,
+  )
+
+  return (
+    <Fragment>
+      <a href={href} className={displayLinkClassName} aria-label='play button' style={style}>
+        <VideoDisplayIcon />
+      </a>
+    </Fragment>
   )
 }
