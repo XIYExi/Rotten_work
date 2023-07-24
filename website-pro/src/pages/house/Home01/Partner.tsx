969507c3a0189cd50a183ac5f656eb6cec521200
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-24 11:57:17 +0800

    add:合伙人页面

---
+++ website-pro/src/pages/house/Home01/Partner.tsx
@@ -0,0 +1,22 @@
+/**
+ * @Description: 合作者页面
+ * @Author: xiye
+ * @date: 2023/7/24
+ */
+import React from "react";
+
+
+export default function PartnerPage() {
+
+  return(
+    <section className='pt-[80px] lg:pt-[120px] pb-[80px] lg:pb-[120px]'>
+      <div className='my-container mx-auto'>
+        <div className='grid grid-cols-12'>
+          <div className='col-span-12'>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
