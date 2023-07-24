d5ef6156c400abf71957109922e5709ad9d1daba
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-24 15:36:46 +0800

    add：提交blogs数据

---
+++ website-pro/src/assets/data/BlogsData.ts
@@ -0,0 +1,20 @@
+const Blogs = [
+  {
+    href: '#',
+    src: '1',
+    meta: 'James Alber on 22 December,21',
+    title:'Tip’s about Real Estate Recent Conditions from Agent.',
+    desc: 'Properties are most budget friendly so you have are opportunity to find are the best the best...'
+  },
+  {
+    href:'#',
+    src:'2',
+    meta: 'Shohel Gyes on 21 December,21',
+    title:'Importance of Build quality of modern Real Estate.',
+    desc: 'Properties are most budget friendly so you have are opportunity to find are the best the best...'
+  }
+]
+
+export default {
+  Blogs: Blogs
+}
