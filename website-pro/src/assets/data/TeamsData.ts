4118da09a0ca9b973d54d523c999ca5b73ba19d2
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-24 15:37:58 +0800

    add：添加teams数据

---
+++ website-pro/src/assets/data/TeamsData.ts
@@ -0,0 +1,43 @@
+const Teams = [
+  {
+    href: '#',
+    src: '1',
+    title: 'Albert\'s Smith',
+    desc: 'Real Estate Agent',
+    facebook: '#',
+    twitter: '#',
+    ins:'#'
+  },
+  {
+    href: '#',
+    src: '2',
+    title: 'Amelia Margaret',
+    desc: 'Real Estate Broker',
+    facebook: '#',
+    twitter: '#',
+    ins:'#'
+  },
+  {
+    href: '#',
+    src: '3',
+    title: 'Stephen Kelvin',
+    desc: 'Real Estate Agent',
+    facebook: '#',
+    twitter: '#',
+    ins:'#'
+  },
+  {
+    href: '#',
+    src: '4',
+    title: 'Michael Richard',
+    desc: 'Real Estate Broker',
+    facebook: '#',
+    twitter: '#',
+    ins:'#'
+  },
+]
+
+
+export default {
+  Teams: Teams,
+}
