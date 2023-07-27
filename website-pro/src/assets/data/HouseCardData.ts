c9acf8a54883f3fd26713f121778d9f148def303
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-27 10:01:19 +0800

    add：添加数据

---
+++ website-pro/src/assets/data/HouseCardData.ts
@@ -0,0 +1,43 @@
+const HouseCardData = [
+  {
+    title: 'Casel de Philadio.',
+    desc: '4356 Riverside Drive,Central Town,Augusta,Georgia',
+    price: 255300,
+    src: '1'
+  },
+  {
+    title: 'Aronno Bilash.',
+    desc: '4356 Riverside Drive,Central Town,Augusta,Georgia',
+    price: 255300,
+    src: '2'
+  },
+  {
+    title: 'Apartment de Villa.',
+    desc: '4356 Riverside Drive,Central Town,Augusta,Georgia',
+    price: 255300,
+    src: '1'
+  },
+  {
+    title: 'Casel de Philadio.',
+    desc: '4356 Riverside Drive,Central Town,Augusta,Georgia',
+    price: 255300,
+    src: '2'
+  },
+  {
+    title: 'Aronno Bilash.',
+    desc: '4356 Riverside Drive,Central Town,Augusta,Georgia',
+    price: 255300,
+    src: '1'
+  },
+  {
+    title: 'Apartment de Villa.',
+    desc: '4356 Riverside Drive,Central Town,Augusta,Georgia',
+    price: 255300,
+    src: '2'
+  },
+]
+
+
+export default {
+  Cards: HouseCardData
+}
