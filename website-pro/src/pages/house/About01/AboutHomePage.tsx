f59bca9bf78599c0e87299afe79253071e98826f
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-07-30 11:03:31 +0800

    add：添加about页面入口和首页

---
+++ website-pro/src/pages/house/About01/AboutHomePage.tsx
@@ -0,0 +1,8 @@
+
+
+export default function ABoutHomePage() {
+
+  return(
+    <section></section>
+  )
+}
---
+++ website-pro/src/pages/house/About01/index.tsx
@@ -0,0 +1,19 @@
+/**
+ * @Description: about01页面入口
+ * @Author: xiye
+ * @date: 2023/7/30
+ */
+import React from "react";
+import AboutHomePage from "./AboutHomePage";
+
+export default function About01Index() {
+
+  return(
+    <>
+      <AboutHomePage />
+
+
+
+    </>
+  )
+}
