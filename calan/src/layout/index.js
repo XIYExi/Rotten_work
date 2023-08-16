f9fb4ae9895f3806d1e11a510ac55a0049d3a5ca
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-08-16 17:03:59 +0800

    update：没几把用，还是写在react里面

--- calan/src/layout/index.js
+++ calan/src/layout/index.js
@@ -1,12 +1,29 @@
 import {Outlet} from "react-router-dom";
+import {useCallback, useEffect} from "react";
+const Creator = require('../func/Creator');
 
 
 export default function LayoutIndex(){
 
-  return(
-    <div>
+    const handleGenerateDemo = () => {
+        (async () => {
+           await new Creator().generateWithDom().generate('test');
+        })()
 
-      layout
+
+    }
+
+  return(
+    <div className='relative'>
+
+        <div
+            onClick={handleGenerateDemo}
+            className='z-[999] absolute cursor-pointer top-0 left-0 py-2 px-10 text-[18px] font-[500]  transition-all duration-300 bg-black text-white border-2 hover:bg-white hover:text-black'>
+            Generate Demo
+        </div>
+
+
+      <div id='test'></div>
 
       <Outlet />
 
--- calan/src/router/index.js
+++ calan/src/router/index.js
@@ -2,10 +2,14 @@ import App from "../App";
 import HeadersIndex from "../pages/headers";
 import ContentsIndex from "../pages/contents";
 import {createHashRouter} from "react-router-dom";
+import LayoutIndex from "../layout";
 
 
 const router = [
-
+  {
+    path: '/layout',
+    element: <LayoutIndex />
+  },
   {
     path: '/',
     element: <App />,
