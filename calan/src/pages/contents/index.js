151a23d5047fbeada0e95312250fd9cff5026f75
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-08-25 18:50:57 +0800

    update：稍微修改

--- calan/src/pages/contents/index.js
+++ calan/src/pages/contents/index.js
@@ -2,6 +2,27 @@ import {createElement, lazy, Suspense, useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import DisplayBoxComponent from "../../components/DisplayBox";
 
+const transfer = [
+    {
+        id: '1',
+        title: 'Easy to Use',
+        desc: 'Components in CaLan are designed to be easily integrated into any design. All components are in the same style and are easily integrated into start-up landing page projects.',
+        active: true,
+    },
+    {
+        id: '1',
+        title: 'Easy to Use',
+        desc: 'Components in CaLan are designed to be easily integrated into any design. All components are in the same style and are easily integrated into start-up landing page projects.',
+        active: false
+    },
+    {
+        id: '1',
+        title: 'Easy to Use',
+        desc: 'Components in CaLan are designed to be easily integrated into any design. All components are in the same style and are easily integrated into start-up landing page projects.',
+        active: false
+    }
+]
+
 const Content02 = lazy(() => import('./contents02'))
 export default function ContentsIndex(){
 
@@ -15,7 +36,7 @@ export default function ContentsIndex(){
       <Suspense fallback={<div>loading...</div>}>
         <DisplayBoxComponent title={`content-${param.id}`}>
           {
-            createElement(lazy(() => import(`./contents${param.id}`)))
+            createElement(lazy(() => import(`./contents${param.id}`)), {})
           }
         </DisplayBoxComponent>
       </Suspense>
