d332ec10286773358e783e424441c18eaec0527e
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-08-29 10:55:50 +0800

    add：添加content21

--- calan/src/pages/contents/contents21.js
+++ calan/src/pages/contents/contents21.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import 'video-react/dist/video-react.css';
+import {Player, BigPlayButton} from 'video-react';
+import poster from '../../assets/images/contents/contents21-poster.png';
 
 
 export default function ContentComponent21(){
@@ -6,9 +9,17 @@ export default function ContentComponent21(){
     return(
         <div className='relative w-full mx-auto lg:px-[165px] lg:py-[80px]'>
 
-            <video>
-
-            </video>
+            <Player
+                className='mx-auto'
+                fluid={false}
+                width={1110}
+                height={624}
+                playsInline
+                poster={poster}
+                src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+            >
+                <BigPlayButton position="center" />
+            </Player>
 
         </div>
     )
