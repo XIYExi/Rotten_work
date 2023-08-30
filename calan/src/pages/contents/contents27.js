3835801601967d36f9580c4f7a203f21897c573d
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-08-30 14:03:56 +0800

    add：提交content27

---
+++ calan/src/pages/contents/contents27.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+export default function ContentComponent27(){
+
+
+    return(
+        <div className='mx-auto relative w-full'>
+            
+        </div>
+    )
+}
