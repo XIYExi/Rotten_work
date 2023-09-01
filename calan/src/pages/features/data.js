6941566df1b1fdf44d7afac5617be61bccb08eb9
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-09-01 19:51:12 +0800

    update:更新features props列表

--- calan/src/pages/features/data.js
+++ calan/src/pages/features/data.js
@@ -1,9 +1,15 @@
 import features1 from './features1/data';
 import features2 from './features2/data';
+import features3 from './features3/data';
+import features4 from './features4/data';
+import features5 from './features5/data';
 
 const featuresTemplate = [
     features1,
     features2,
+    features3,
+    features4,
+    features5,
 ]
 
 const data =featuresTemplate.map((v, index) => {
