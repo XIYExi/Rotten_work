1bdb08bcf479681b1854a3100352c8b4bb6d558c
zhouxi <10045835+zhouxisad@user.noreply.gitee.com>
2023-09-03 15:33:07 +0800

    update: 提交features入口

--- calan/src/pages/features/data.js
+++ calan/src/pages/features/data.js
@@ -3,6 +3,8 @@ import features2 from './features2/data';
 import features3 from './features3/data';
 import features4 from './features4/data';
 import features5 from './features5/data';
+import features6 from './features6/data';
+import features7 from './features7/data';
 
 const featuresTemplate = [
     features1,
@@ -10,6 +12,8 @@ const featuresTemplate = [
     features3,
     features4,
     features5,
+    features6,
+    features7,
 ]
 
 const data =featuresTemplate.map((v, index) => {
